bb68de70dfbdb9a80c3b4309c56ebba401e66f9e
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-03-29 21:34:29 +0100

    framework:: Added basic concept.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
 
+int source() { return 0; }
 
-int main() { return 0; };
+int filter(int input) { return input; }
+
+void sink(int input) { std::cout << input << '\n'; }
+
+int main() {
+
+  int channel1, channel2;
+  channel1 = source();
+  channel2 = filter(channel1);
+  sink(channel2);
+
+  return 0;
+}
