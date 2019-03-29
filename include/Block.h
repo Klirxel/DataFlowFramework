7848ac3d27a0432bdb690f6bd95c327c8e117edc
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-03-29 23:18:02 +0100

    block::feat:: Added executor concept.

--- include/Block.h
+++ include/Block.h
@@ -3,23 +3,46 @@
 
 namespace df {
 
-template <typename OPERATOR, typename... INPUT> class Block {
+template <typename OPERATOR, typename... INPUT> class InvokeExecutor {
 
 public:
   using OUTPUT = std::invoke_result_t<OPERATOR, INPUT...>;
 
-  constexpr Block(OPERATOR &op) noexcept : op_(op){};
-  
-  inline OUTPUT operator()(const INPUT &... input) noexcept {
+  constexpr InvokeExecutor(OPERATOR& op) noexcept: op_(op){};
+
+  constexpr OUTPUT operator()(const INPUT &... input) noexcept {
     return std::invoke(op_, input...);
+  };
+
+  constexpr OUTPUT operator()(INPUT &&... input) noexcept {
+    return std::invoke(op_, std::forward(input)...);
+  };
+
+private:
+  OPERATOR& op_;
+};
+
+template <typename OPERATOR, typename EXECUTOR, typename... INPUT> class Block {
+
+public:
+  using OUTPUT = std::invoke_result_t<OPERATOR, INPUT...>;
+
+  constexpr Block(OPERATOR &op) noexcept : executor_(op){};
+
+  inline OUTPUT operator()(const INPUT &... input) noexcept {
+    return executor_(input...);
   }
 
   inline OUTPUT operator()(INPUT &&... input) noexcept {
-    return std::invoke(op_, std::forward(input)...);
+    return executor_(std::forward(input)...);
   }
 
 private:
-  OPERATOR &op_;
+  EXECUTOR executor_;
 };
+
+template <typename OPERATOR, typename... INPUT>
+using SyncBlock =
+    Block<OPERATOR, InvokeExecutor<OPERATOR, INPUT...>, INPUT...>;
 
 } // namespace df
