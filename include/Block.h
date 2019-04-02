7f694d1b99f8d0518e4bc6fd4f4fd85861cd1344
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-04-02 20:49:46 +0200

    Block:: Refactoring.
    Seperated declaration and definiton.

--- include/Block.h
+++ include/Block.h
@@ -10,25 +10,17 @@ template <typename T_IN, typename OPERATOR, typename T_OUT>
 class Block : public BlockIf {
 
 public:
-  Block(Channel<T_IN> &chanIn, OPERATOR &op, Channel<T_OUT> &chanOut)
-      : chanIn_(chanIn), op_(op), chanOut_(chanOut) {
-    chanIn_.attachSinkBlock(this);
-  };
+  Block(Channel<T_IN> &chanIn, OPERATOR &op, Channel<T_OUT> &chanOut);
 
-  bool readyForExecution() override { return not chanIn_.empty(); };
+  bool readyForExecution() override;
 
-  void execute() override {
-    T_IN input = chanIn_.pop();
-    T_OUT output = op_(std::move(input));
-    chanOut_.push(std::move(output));
-  };
+  void execute() override;
 
 private:
   Channel<T_IN> &chanIn_;
   OPERATOR &op_;
   Channel<T_OUT> &chanOut_;
 };
-
 
 } // namespace df
 
