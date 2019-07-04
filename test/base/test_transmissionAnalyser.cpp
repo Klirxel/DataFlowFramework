cec1fa9668d49650ce38adf3dc20a08c1315ab6b
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-07-04 21:06:39 +0200

    base: Added TransmissionAnalyser.

---
+++ test/base/test_transmissionAnalyser.cpp
@@ -0,0 +1,81 @@
+#define BOOST_TEST_MODULE testTransmissionAnalyser
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "TransmissionAnalyser.h"
+
+using namespace df::base;
+
+BOOST_AUTO_TEST_CASE(DefaultCtorCall)
+{
+    const TransmissionAnalyser ta {};
+
+    BOOST_CHECK_EQUAL(ta.callsCopyCtor, 0);
+    BOOST_CHECK_EQUAL(ta.callsMoveCtor, 0);
+    BOOST_CHECK_EQUAL(ta.callsCopyAssOp, 0);
+    BOOST_CHECK_EQUAL(ta.callsMoveAssOp, 0);
+    BOOST_CHECK_EQUAL(TransmissionAnalyser::callsDestructor, 0);
+}
+
+BOOST_AUTO_TEST_CASE(CopyCtorCall)
+{
+    TransmissionAnalyser taBase {};
+    const int offset = 10;
+    taBase.calls.fill(offset);
+    TransmissionAnalyser::callsDestructor = offset;
+
+    const TransmissionAnalyser ta { taBase };
+
+    BOOST_CHECK_EQUAL(ta.callsCopyCtor, 1 + offset);
+    BOOST_CHECK_EQUAL(ta.callsMoveCtor, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsCopyAssOp, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsCopyAssOp, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsMoveAssOp, 0 + offset);
+    BOOST_CHECK_EQUAL(TransmissionAnalyser::callsDestructor, 0 + offset);
+}
+
+BOOST_AUTO_TEST_CASE(MoveCtorCall)
+{
+    TransmissionAnalyser taBase {};
+    const int offset = 10;
+    taBase.calls.fill(offset);
+    TransmissionAnalyser::callsDestructor = offset;
+
+    TransmissionAnalyser ta { std::move(taBase) };
+
+    BOOST_CHECK_EQUAL(ta.callsCopyCtor, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsMoveCtor, 1 + offset);
+    BOOST_CHECK_EQUAL(ta.callsCopyAssOp, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsMoveAssOp, 0 + offset);
+    BOOST_CHECK_EQUAL(TransmissionAnalyser::callsDestructor, 0 + offset);
+}
+
+BOOST_AUTO_TEST_CASE(CopyAssOpCall)
+{
+    TransmissionAnalyser taBase {};
+    const int offset = 10;
+    taBase.calls.fill(offset);
+    TransmissionAnalyser::callsDestructor = offset;
+
+    TransmissionAnalyser ta {};
+    ta = taBase;
+
+    BOOST_CHECK_EQUAL(ta.callsCopyCtor, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsMoveCtor, 0 + offset);
+    BOOST_CHECK_EQUAL(ta.callsCopyAssOp, 1 + offset);
+    BOOST_CHECK_EQUAL(ta.callsMoveAssOp, 0 + offset);
+    BOOST_CHECK_EQUAL(TransmissionAnalyser::callsDestructor, 0 + offset);
+}
+
+BOOST_AUTO_TEST_CASE(DestructorCall)
+{
+    const int offset = 10;
+
+    TransmissionAnalyser::callsDestructor = offset;
+
+    {
+        TransmissionAnalyser taBase {};
+    }
+
+    BOOST_CHECK_EQUAL(TransmissionAnalyser::callsDestructor, 1 + offset);
+}
