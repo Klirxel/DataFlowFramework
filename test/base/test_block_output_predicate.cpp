6e39c37fe6fac6c02dc14e11207e93e004ba0189
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-10-30 00:06:27 +0100

    test:base: Added test_block_output_predicate.

---
+++ test/base/test_block_output_predicate.cpp
@@ -0,0 +1,52 @@
+#define BOOST_TEST_MODULE test_block_output_predicate
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "Block.h"
+#include "Channels.h"
+#include "Executor.h"
+
+using namespace df::base;
+
+template <typename... T>
+struct OutputOnlyEven {
+    constexpr std::array<bool, sizeof...(T)> operator()(const T&... inputPack) const
+    {
+        auto isEven = [](const auto& number) { return number % 2 == 0; };
+        return { isEven(inputPack)... };
+    };
+};
+
+constexpr std::tuple<int> passThrough(int val) noexcept
+{
+    return std::tuple<int> { val };
+}
+
+BOOST_AUTO_TEST_CASE(BasicOutputPredicate)
+{
+    Channel<int> chanIn, chanOut;
+
+    Executor executor;
+
+    Block block {
+        ChannelBundle { chanIn },
+        passThrough,
+        ChannelBundle { chanOut },
+        executor,
+        OutputOnlyEven<int> {}
+    };
+
+    const size_t cycles = 101;
+
+    for (size_t cycle = 0; cycle < cycles; ++cycle) {
+        chanIn.push(cycle);
+    };
+
+    const size_t cyclesEven = cycles % 2 == 0 ? cycles / 2 : cycles / 2 + 1;
+
+    BOOST_CHECK_EQUAL(chanOut.size(), cyclesEven);
+
+    for (size_t i = 0; i < cyclesEven; ++i) {
+        BOOST_CHECK_EQUAL(chanOut.pop().value() % 2, 0);
+    }
+}
