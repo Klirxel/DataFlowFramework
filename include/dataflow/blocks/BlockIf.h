baf5c79a15aeb50918c8b80d05b9794b00e732d9
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-11-21 18:17:18 +0100

    Blocks: Documenting BockIf.h

--- include/dataflow/blocks/BlockIf.h
+++ include/dataflow/blocks/BlockIf.h
@@ -2,10 +2,27 @@
 
 namespace dataflow::blocks {
 
+/**
+ * @brief Block interface class.
+ *
+ * @details
+ * - Blocks are the functional objects in an dataflow
+ *   network. A block can be attached to input- and
+ *   output-channels and an invocable object the 
+ *   block operator. 
+ * - There are mainly three types of blocks, 
+ *   dataflow::blocks::Block, dataflow::blocks::GeneratorBlock,
+ *   dataflow::blocks::SinkBlock.
+ *   (Standard) blocks have input- and ouput-channels and
+ *   are processing the input data to generate output data.
+ *   GeneratorBlocks only have output channels
+ *   and generate only data. SinkBlocks have only
+ *   input channels and only consume data.
+ */
 class BlockIf {
 public:
     virtual bool readyForExecution() const = 0;
     virtual void execute() = 0;
 };
 
-} // namespace df
+} //namespace dataflow::blocks
