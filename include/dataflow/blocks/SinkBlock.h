18136b060c70181f6cd4c35679a7d8466cc52003
Kurt <kurt.schwenk@gmx.de>
2019-12-07 16:49:18 +0100

    Block: Added documentation for the SinkBlock.

--- include/dataflow/blocks/SinkBlock.h
+++ include/dataflow/blocks/SinkBlock.h
@@ -11,12 +11,32 @@ using namespace dataflow::executors;
 
 namespace dataflow::blocks {
 
+/**
+ * @note
+ * - Primary template for the Sinkblock.
+ * - Technical helper functions.
+ * - Has no implementation.
+ * - Implementation only by partial specialisations.
+ * - The reason using a partial specialization for
+ *   implementation is that only one template parameter pack is
+ *   allowed to be used for primary templates. 
+ */
 template <typename CHAN_BUNDLE_IN, typename OPERATOR>
 class SinkBlock {
 public:
     SinkBlock(CHAN_BUNDLE_IN /*unused*/, OPERATOR& /*unused*/, ExecutorIf&);
 };
 
+/**
+ * @brief (Input-)Block
+ *
+ * @details
+ * - Default implementation of an input block.
+ * - More general information about Blocks see @ref BlockIf.
+ *
+ * @tparam T_IN...   Value types of the input data channels.
+ * @tparam OPERATOR  Function kernel of the block
+ */
 template <typename... T_IN, typename OPERATOR>
 class SinkBlock<ChannelBundle<T_IN...>,
     OPERATOR> : public BlockIf {
