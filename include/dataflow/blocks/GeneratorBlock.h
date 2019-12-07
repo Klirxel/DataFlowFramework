7fee31083ecd76b5d8bbb346e3d81ce7daa5bf41
Kurt <kurt.schwenk@gmx.de>
2019-12-07 12:08:40 +0100

    block: Added doc and static type checking to GeneratorBlock.

--- include/dataflow/blocks/GeneratorBlock.h
+++ include/dataflow/blocks/GeneratorBlock.h
@@ -16,15 +16,39 @@ namespace dataflow::blocks {
 
 constexpr size_t inf = 0;
 
+/**
+ * @note
+ * - Primary template for GeneratorBlock.
+ * - Technical helper functions.
+ * - Has no implementation.
+ * - Implementation only by partial specialisations.
+ * - The reason using a partial specialization for
+ *   implementation is that only one template parameter pack is
+ *   allowed to be used for primary templates. 
+ */
 template <typename OPERATOR, typename CHAN_BUNDLE_OUT>
 class GeneratorBlock {
 public:
     GeneratorBlock(OPERATOR& /*unused*/, CHAN_BUNDLE_OUT /*unused*/);
 };
 
+/**
+ * @brief (Output-)Block
+ *
+ * @details
+ * - Output block that periodically produces output data. 
+ * - More general information about Blocks see @ref BlockIf.
+ *
+ * @tparam OPERATOR  Function kernel of the block
+ * @tparam T_OUT...  Value types of the output data channels.
+ *
+ */
 template <typename OPERATOR, typename... T_OUT>
 class GeneratorBlock<
     OPERATOR, ChannelBundle<T_OUT...>> : public BlockIf {
+
+    static_assert(std::is_invocable_r_v<std::tuple<T_OUT...>, OPERATOR>,
+        "Operator is not consistent with the input or/and output parameters!");
 
 public:
     GeneratorBlock(OPERATOR& op, ChannelBundle<T_OUT...> outputChannels);
