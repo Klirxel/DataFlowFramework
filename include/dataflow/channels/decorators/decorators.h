510f4a7fca1b3eb7bb3341eee159e82db692c372
Kurt <kurt.schwenk@gmx.de>
2019-12-18 18:39:02 +0100

    channels: Added namespace doc for decorators.

--- include/dataflow/channels/decorators/decorators.h
+++ include/dataflow/channels/decorators/decorators.h
@@ -1,38 +1,22 @@
 #pragma once
 
-#include <type_traits>
+#include "ChannelAlwaysDataAvailableDecorator.h"
+#include "ChannelThreadSafetyDecorator.h" 
 
-#include "../blocks/BlockIf.h"
-#include "ChannelIf.h"
 
-using namespace dataflow::blocks;
+/** @namespace dataflow::channels::decorators
+ *
+ * @brief Channel feature wrappers. 
+ *
+ * @details
+ * - Can be used for adding features to classes that 
+ *   are derived form ChannelIf.
+ */
+namespace dataflow::channels::decorators
+{
+} // namespace dataflow::channels::decorators
 
-namespace dataflow::channels {
 
-template <class CHANNEL>
-class ChannelAlwaysDataAvailableDecorator : public ChannelIf<typename CHANNEL::ValueType> {
 
-public:
-    using ValueType = typename CHANNEL::ValueType;
 
-    static_assert(std::is_base_of_v<ChannelIf<ValueType>, CHANNEL>, "CHANNEL is not derived from ChannelIf<T>");
-    static_assert(std::is_default_constructible_v<CHANNEL>, "CHANNEL is not default constructible.");
-    static_assert(std::is_default_constructible_v<ValueType>, "ValueType is not default constructible.");
 
-    void attachSinkBlock(BlockIf* /*block*/) override;
-    void attachSourceBlock(BlockIf* /*block*/) override;
-
-    std::optional<ValueType> pop() override;
-    void push(ValueType&& /*data*/) override;
-    [[nodiscard]] bool dataAvailable() const override;
-    [[nodiscard]] bool dataAssignable() const override;
-    [[nodiscard]] constexpr std::size_t size() const override;
-    [[nodiscard]] constexpr std::size_t max_size() const override;
-
-private:
-    CHANNEL chan_;
-};
-
-} // namespace df
-
-#include "impl/ChannelAlwaysDataAvailableDecorator.hpp"
