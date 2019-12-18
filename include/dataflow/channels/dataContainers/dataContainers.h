de0872867014e98ff6f80fd5ce5a194f4312a711
Kurt <kurt.schwenk@gmx.de>
2019-12-18 18:08:42 +0100

    channels: Added header file for namespace  dataContainers.

---
+++ include/dataflow/channels/dataContainers/dataContainers.h
@@ -0,0 +1,13 @@
+#include "ChannelCircleBufContainer.h"
+
+/** @namespace dataflow::channels::dataContainers
+ *
+ * @brief Channel buffer types. 
+ *
+ * @details
+ * - Buffer types used by the implementations of
+ *   ChannelIf. 
+ */
+namespace dataflow::channels::dataContainers {
+
+} // namespace dataflow::channels::dataContainers
