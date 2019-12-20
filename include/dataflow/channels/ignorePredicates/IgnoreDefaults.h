491c4befb895476e05a3f4429b6ecb2dbacbf28b
Kurt <kurt.schwenk@gmx.de>
2019-12-20 16:32:01 +0100

    channels: Added type requirements to IgnoreDefaults.

--- include/dataflow/channels/ignorePredicates/IgnoreDefaults.h
+++ include/dataflow/channels/ignorePredicates/IgnoreDefaults.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "../../tools/is_compareable.h"
+
 namespace dataflow::channels::ignorePredicates {
 
 struct IgnoreDefaults {
 
-    template <typename ValueType>
+    template <typename ValueType,
+        typename = std::enable_if_t<std::is_default_constructible_v<ValueType>>,
+        typename = std::enable_if_t<tools::is_compareable_v<ValueType, ValueType>>>
     [[nodiscard]] bool operator()([[maybe_unused]] const ValueType& val) const;
 };
 
--- include/dataflow/channels/impl/IgnoreDefaults.hpp
+++ include/dataflow/channels/impl/IgnoreDefaults.hpp
@@ -2,7 +2,9 @@
 
 namespace dataflow::channels::ignorePredicates {
 
-template <typename ValueType>
+template <typename ValueType,
+    typename = std::enable_if_t<std::is_default_constructible_v<ValueType>>,
+    typename = std::enable_if_t<tools::is_compareable_v<ValueType, ValueType>>>
 [[nodiscard]] bool IgnoreDefaults::operator()(const ValueType& val) const
 {
     return val == ValueType {};
