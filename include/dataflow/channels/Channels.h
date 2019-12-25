d9f9c7734f448c05572c87929934bd6016dcf21e
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-12-25 12:46:13 +0100

    channels: Added ChannelAdTs.h.

--- include/dataflow/channels/Channels.h
+++ include/dataflow/channels/Channels.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include "Channel.h"
-#include "ChannelCircleBuf.h"
-#include "decorators/ChannelAlwaysDataAvailableDecorator.h"
-#include "decorators/ChannelThreadSafetyDecorator.h"
-#include "ignorePredicates/IgnoreDefaults.h"
-
 #include "ChannelAlwaysDataAvailable.h"
 #include "ChannelCircleBufThreadSafe.h"
 #include "ChannelDataAvailableNoTrigger.h"
@@ -14,8 +8,5 @@
 #include "ChannelThreadSave.h"
 
 namespace dataflow::channels {
-
-template <typename T>
-using ChannelAdTs = decorators::ChannelThreadSafetyDecorator<ChannelAlwaysDataAvailable<T>>;
 
 } // namespace dataflow::channels
