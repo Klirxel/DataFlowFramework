c189f30e6554820a06ae97052747cf34cc8323ba
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-12-25 12:51:18 +0100

    channels: Removed unnecessary definitions.

--- include/dataflow/channels/ChannelDataAvailableNoTrigger.h
+++ include/dataflow/channels/ChannelDataAvailableNoTrigger.h
@@ -1,25 +1,11 @@
 #pragma once
 
 #include "Channel.h"
-#include "ChannelCircleBuf.h"
 #include "decorators/ChannelAlwaysDataAvailableDecorator.h"
-#include "decorators/ChannelThreadSafetyDecorator.h"
-#include "ignorePredicates/IgnoreDefaults.h"
-
-#include "ChannelAlwaysDataAvailable.h"
-#include "ChannelThreadSafeIgnoreDefaults.h"
-#include "ChannelThreadSafeNoTrigger.h"
-#include "ChannelThreadSave.h"
 
 namespace dataflow::channels {
 
 template <typename T>
 using ChannelAlwaysDataAvailableNoTrigger = decorators::ChannelAlwaysDataAvailableDecorator<Channel<T, TriggerPolicy::triggerNone, TriggerPolicy::triggerNone>>;
-
-template <typename T>
-using ChannelAdTs = decorators::ChannelThreadSafetyDecorator<ChannelAlwaysDataAvailable<T>>;
-
-template <typename T, std::size_t Size>
-using ChannelCircleBufThreadSafe = decorators::ChannelThreadSafetyDecorator<ChannelCircleBuf<T, Size>>;
 
 } // namespace dataflow::channels
