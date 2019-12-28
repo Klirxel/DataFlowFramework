5b3f17b823c9b8ea7828f234aeaaa99d875208fb
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-12-28 12:49:59 +0100

    executors:tech_debt: Added executors.h.

---
+++ include/dataflow/executors/executors.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Executor.h"
+#include "ExecutorAsync.h"
+#include "ExecutorMultithread.h"
+
+/** @namespace dataflow::executors
+ *
+ * @brief Executing entities of the dataflow framework. 
+ *
+ * @details
+ * - With executors the execution behaviour of the dataflow framework
+ *   is implemented.
+ */
+namespace dataflow::executors {
+} // namespace dataflow::executors
