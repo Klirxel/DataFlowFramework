a8f7916a5b44b51307382a5ace03d9ce11b5b616
Kurt Schwenk <kurt.schwenk@gmx.de>
2019-12-31 11:24:39 +0100

    executors: Documentaiton of Executor.

--- include/dataflow/executors/Executor.h
+++ include/dataflow/executors/Executor.h
@@ -4,12 +4,19 @@
 
 namespace dataflow::executors {
 
+/**
+ * @brief Sequential executor.
+ *
+ * @details
+ * - Sequential (not parallel) execution of tasks.
+ * - No thread safe channels required. 
+ */
 class Executor : public ExecutorIf {
 
 public:
     inline void execute(std::function<void(void)>&& task, std::mutex& taskLock) override;
 };
 
-} //ns
+} //namespace dataflow::executors
 
 #include "impl/Executor.hpp"
