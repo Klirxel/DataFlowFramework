d8151b2344f8826a7e427d7ebdf285d755a890be
Kurt Schwenk <kurt.schwenk@gmx.de>
2020-01-01 18:30:34 +0100

    tools: Added tools.h.

---
+++ include/dataflow/tools/tools.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "TransmissionAnalyser.h"
+#include "is_compareable.h"
+
+/** @namespace dataflow::tools
+ *
+ * @brief Tools and helping functions of the dataflow framework
+ */
+namespace dataflow::tools {
+
+} // namespace dataflow:tools
