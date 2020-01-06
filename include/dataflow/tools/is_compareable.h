ddfb6639c727f0fe6157756657a6f0a3ec608341
Kurt Schwenk <kurt.schwenk@gmx.de>
2020-01-06 16:36:45 +0100

    tools: Documentation of is_comperable.

--- include/dataflow/tools/is_compareable.h
+++ include/dataflow/tools/is_compareable.h
@@ -4,10 +4,16 @@
 
 namespace dataflow::tools {
 
+/**
+ * @brief type trait: checks if types are compareable 
+ */
 template <typename T, typename U>
 struct is_compareable : impl::is_compareable_impl<T, U> {
 };
 
+/**
+ * @brief helper variable template
+ */
 template <typename T, typename U>
 static constexpr bool is_compareable_v = is_compareable<T, U>::value;
 
