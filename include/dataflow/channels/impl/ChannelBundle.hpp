610971688188185fdd381384b6f92b42dd82e084
Kurt Schwenk <kurt.schwenk@gmx.de>
2020-01-26 17:19:37 +0100

    repo: Added license disclaimers to files.

--- include/dataflow/channels/impl/ChannelBundle.hpp
+++ include/dataflow/channels/impl/ChannelBundle.hpp
@@ -1,3 +1,10 @@
+/*
+ *          Copyright Schwenk, Kurt <Kurt.Schwenk@gmx.de> 2020
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 #include <utility>
 
 #include "../ChannelBundle.h"
