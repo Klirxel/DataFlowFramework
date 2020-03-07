787f9959ec1802bd27660e7f8db29d7c2eab1277
Kurt Schwenk <kurt.schwenk@gmx.de>
2020-03-07 11:40:00 +0100

    examples: Renamed simpleOutputFilter -> simpleBlockOutputPredicate.
    Reason: better readability.

--- examples/simpleBlockOutputPredicate.cpp
+++ examples/simpleBlockOutputPredicate.cpp
@@ -9,47 +9,49 @@
 #include <iostream>
 #include <utility>
 
-//Simple output filter example.
+//Simple block output predicate example.
 //
 //  DataflowGraph:
 //
-//                                               ---chan2---> ConsoleWriterEvenNumbers
-//  NumberGenerator  --chan1--> EvenOddSeperator
-//                                               ---chan3---> ConsoleWriterOddNumbers
+//                                                  ---chan_even---> ConsoleWriterEvenNumbers
+//  NumberGenerator  --chan_num--> EvenOddSeperator
+//                                                  ---chan_odd----> ConsoleWriterOddNumbers
 //
 //  Description:
-//  With an output filter predicate it can be controlled if output from an kernel
+//  With an output predicate of an block
+//  it can be controlled if output from an kernel
 //  of an block should be written to an output channel.
 //
 int main()
 {
     //Definition Channels
-    Channel<int> chan1;
-    Channel<int> chan2;
-    Channel<int> chan3;
+    Channel<int> chan_num;
+    Channel<int> chan_even;
+    Channel<int> chan_odd;
 
     //Definiton Executor
     ExecutorSeq executor;
 
     //Definiton NumberGenerator1
     auto counter = [count = 0]() mutable { return count++; };
-    GeneratorBlock numberGenerator { counter, ChannelBundle { chan1 } };
+    GeneratorBlock numberGenerator { counter, ChannelBundle { chan_num } };
 
     //Definition SeperateEvenOdd
-    auto doubler = [](auto val) { return std::tuple { val, val }; };
+    auto doubler = [](auto number) { return std::tuple { number, number }; };
 
     auto isEven = [](auto number) { return number % 2 == 0; };
     auto isOdd = [](auto number) { return number % 2 == 1; };
 
-    Block EvenOddSeperator { ChannelBundle { chan1 }, doubler, ChannelBundle { chan2, chan3 },
-        executor, OutputPredicate{isEven, isOdd} };
+    Block EvenOddSeperator { ChannelBundle { chan_num }, doubler,
+      ChannelBundle { chan_even, chan_odd }, executor,
+      OutputPredicate{isEven, isOdd} };
 
     //Definition ConsoleWriterEven/OddNumber
     auto writerEven = [](auto val) { std::cout << "Output even number: " << val << '\n'; };
-    SinkBlock consoleWriterEvenNumbers { ChannelBundle { chan2 }, writerEven, executor };
+    SinkBlock consoleWriterEvenNumbers { ChannelBundle { chan_even }, writerEven, executor };
 
     auto writerOdd = [](auto val) { std::cout << "Output odd number: " << val << '\n'; };
-    SinkBlock consoleWriterOddNumbers { ChannelBundle { chan3 }, writerOdd, executor };
+    SinkBlock consoleWriterOddNumbers { ChannelBundle { chan_odd }, writerOdd, executor };
 
     //Start generators
     const auto period = 10ms;
